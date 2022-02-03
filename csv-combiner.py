243f2fa9f806b44ecde5d5a559224967d4111eed
Meena Muthusubramanian <meena.muthusubramanian@vanderbilt.edu>
2022-02-03 17:40:33 +0530

    Updated

--- csv-combiner.py
+++ csv-combiner.py
@@ -1,26 +1,25 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 import os
 import sys
 
-def csv_combiner(filenames, outputfile='combined_csv.csv'):
-    
+def csv_combiner(filenames):
     if all([filename.endswith('.csv') for filename in filenames]):
         header_done = False
-        with open(outputfile, 'w') as outfile:
-            for fname in filenames:
-                filename = os.path.basename(fname)
-                with open(fname) as infile:
-                    if header_done:
-                        next(infile)
-                    for line in infile:
-                        if not header_done:
-                            outfile.write(line.replace('\n', '') + ',"filename"\n') 
-                            header_done = True  
-                        else:
-                            outfile.write(line.replace('\n', '') + ',"' + filename + '"\n')
+        for fname in filenames:
+            filename = os.path.basename(fname)
+            with open(fname) as infile:
+                if header_done:
+                    next(infile)
+                for line in infile:
+                    if not header_done:
+                        sys.stdout.write(line.replace('\n', '') + ',"filename"\n')    
+                        header_done = True  
+                    else:
+                        sys.stdout.write(line.replace('\n', '') + ',"' + filename + '"\n')
     else:
         print('Unsupported filenames are provided')
 
 if __name__ == "__main__":
-    filenames = sys.argv[1:] or ['fixtures/accessories.csv', 'fixtures/clothing.csv', 'fixtures/household_cleaners.csv']
-    csv_combiner(filenames)
+    filenames = sys.argv[1:]
+    if filenames:
+        csv_combiner(filenames)
